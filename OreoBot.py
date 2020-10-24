b56e96f8bba0e64e31b74322519a88d5a021c9c9
NBorre347 <nathanborrecollege@gmail.com>
2020-10-24 01:06:08 -0500

    Added anti spam and polished anti swearing

--- OreoBot.py
+++ OreoBot.py
@@ -2,21 +2,61 @@ import discord
 import asyncio
 import time
 
-TOKEN = "NzY5Mzg3MjE5MTUyMjczNDE5.X5ORrw.1wrv7kv6bMSdhinwOb7BA_B949w"
+TOKEN = ""
 GUILD = "OreoShunment's Demonhack Server"
 
 client = discord.Client()
 
+kickList = {}
+lastMsg = {}
+cussVariants = ['fuck', 'shit', 'bitch', 'ass']#can add other cuss words
+
 @client.event
 async def on_message(message):
-    pass
+	if message.author == client.user: #don't want bot replying to itself
+		return
+	
+	if message.author in lastMsg and message.content.lower() == lastMsg[message.author]: # Spam filter
+		if message.author in kickList:
+			if kickList[message.author] >= 2:
+				msg = str(message.author) + " has been kicked from this server for spamming."
+				await message.channel.send(msg)
+				#await message.author.kick()
+				kickList.pop(message.author)
+			else:
+				kickList[message.author] = kickList[message.author] + 1
+				msg = str(message.author) + ", don't spam! This is your " + ("first" if kickList[message.author] == 1 else "second") + " warning."
+				await message.channel.send(msg)
+		else:
+			kickList[message.author] = 1
+			msg = str(message.author) + ", don't spam! This is your first warning."
+			await message.channel.send(msg)
+	else:
+		lastMsg[message.author] = message.content.lower()
+        
+	if any(variant in message.content.lower() for variant in cussVariants): # Test for swearing
+		if len(message.author.roles) == 1: #change to check
+			if message.author in kickList:
+				if kickList[message.author] >= 2:
+					msg = str(message.author) + ' has been kicked from this server for cussing too many times.'
+					await message.channel.send(msg)
+					#await message.author.kick()
+					kickList.pop(message.author) #if user is added back, their chance gets reset
+				else:
+					kickList[message.author] = kickList[message.author] + 1
+					msg = str(message.author) + ", don't cuss! This is your " + ("first" if kickList[message.author] == 1 else "second") + " warning."
+					await message.channel.send(msg)
+			else:
+				kickList[message.author] = 1
+				msg = str(message.author) + ", don't cuss! This is your first warning."
+				await message.channel.send(msg)
 
 @client.event
 async def on_ready():
-    for guild in client.guilds:
-        if guild.name == GUILD:
-            break
+	for guild in client.guilds:
+		if guild.name == GUILD:
+			break
 
-    print(client.user, 'connected to guild ', guild.name, '#', guild.id)
+	print(client.user, 'connected to guild ', guild.name, '#', guild.id)
 
 client.run(TOKEN)
